19681535a63be64003c66355bd0a6bdf903faf3a
Paulius J <pauljusk@gmail.com>
2022-12-01 16:55:47 -0600

    some updates

---
+++ Day1_Calorie_Counting/main.js
@@ -0,0 +1,39 @@
+// const fs = require('fs');
+
+// function main(){
+//     fs.readFile('input.txt', 'utf8', function(err, data) {
+//         if (err) throw err;
+//         console.log(data);
+//     });
+// }
+
+// main();
+
+
+
+
+
+const events = require('events');
+const fs = require('fs');
+const readline = require('readline');
+
+(async function processLineByLine() {
+  try {
+    const rl = readline.createInterface({
+      input: fs.createReadStream('input.txt'),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+      console.log(`Line from file: ${line}`);
+    });
+
+    await events.once(rl, 'close');
+
+    console.log('Reading file line by line with readline done.');
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
