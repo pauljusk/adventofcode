23d01b6420634606f67e1767ed34d3bd78c65204
Paulius Juskevicius <paulius.juskevicius@capsim.com>
2022-12-01 22:53:04 -0600

    some updates

--- Day1_Calorie_Counting/main.js
+++ Day1_Calorie_Counting/main.js
@@ -20,15 +20,41 @@ const readline = require('readline');
 (async function processLineByLine() {
   try {
     const rl = readline.createInterface({
-      input: fs.createReadStream('input.txt'),
+      // input: fs.createReadStream('./Day1_Calorie_Counting/input-basic.txt'),
+      input: fs.createReadStream('./Day1_Calorie_Counting/input.txt'),
       crlfDelay: Infinity
     });
 
+    let calSum = 0
+    let calSumMaxJSON = [0, 0, 0]
+    let calSumMaxMin = 0
+
     rl.on('line', (line) => {
-      console.log(`Line from file: ${line}`);
+      if (line == '') {
+
+        if (calSum > calSumMaxMin) {
+          const min = Math.min(...calSumMaxJSON);
+          const index = calSumMaxJSON.indexOf(min);
+          calSumMaxJSON[index] = calSum
+          calSumMaxMin = Math.min(...calSumMaxJSON);
+        }
+        calSum = 0
+      } else {
+        calSum = calSum + Number(line)
+      }
     });
 
     await events.once(rl, 'close');
+
+    if (calSum > calSumMaxMin) {
+      const min = Math.min(...calSumMaxJSON);
+      const index = calSumMaxJSON.indexOf(min);
+    }
+
+    const sum = calSumMaxJSON.reduce((accumulator, value) => {
+      return accumulator + value;
+    }, 0);
+    console.log(`sum: ${sum}`);
 
     console.log('Reading file line by line with readline done.');
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
