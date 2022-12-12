6a0ae7d878a6522da0d64563f6628a61f6e5607a
Paulius Juskevicius <paulius.juskevicius@capsim.com>
2022-12-12 16:31:50 -0600

    some updates

--- day10/index.js
+++ day10/index.js
@@ -1,31 +1,75 @@
-
-const events = require('events');
-const fs = require('fs');
-const readline = require('readline');
+const events = require("events");
+const fs = require("fs");
+const readline = require("readline");
 
 (async function processLineByLine() {
   try {
     const rl = readline.createInterface({
       // input: fs.createReadStream(`${__dirname}/input-basic.txt`),
       input: fs.createReadStream(`${__dirname}/input.txt`),
-      crlfDelay: Infinity
-    });
-    
-    let score = 0
-    let scorePart2 = 0
-
-    rl.on('line', (line) => {
-      // score += scoreMap[line]
-      // scorePart2 += scoreMapPart2[line]
+      crlfDelay: Infinity,
     });
 
-    await events.once(rl, 'close');
+    // Part 1
+    // let sum = 0
+    // let sumObj = {}
+    // let cycle = 1;
+    // let x = 1;
+    // let values = {};
+    // rl.on("line", (line) => {
+    //   const lineArr = line.split(" ");
+    //   if (lineArr[0] == "noop") cycle++;
+    //   else {
+    //     cycle += 2;
+    //     x += Number(lineArr[1]);
+    //   }
+    //   values[cycle] = x;
+    // });
 
+    // await events.once(rl, "close");
 
-    console.log(`score: ${score}`);
-    console.log(`scorePart2: ${scorePart2}`);
+    // for(let i = 20; i <= 220; i+=40){
+    //   let index = (values[i] != undefined)? i : i-1
+    //   const cycleVal = values[index]
+    //   sum+=cycleVal*i
+    //   sumObj[i]=cycleVal*i
+    //   console.log('values[index]',values[index]);
+    // }
+    // console.log(sum);
+    // console.log(sumObj);
 
+    // Part 2
+    let cycle = 1;
+    let x = 1;
+    let results = "";
+    rl.on("line", (line) => {
+      const lineArr = line.split(" ");
+
+      if (cycle >= x && cycle <= x + 2) results += "#";
+      else results += ".";
+
+      cycle++;
+      if (cycle>40) cycle = 1
+      if (lineArr[0] == "addx") {
+        if (cycle  >= x && cycle <= x + 2) results += "#";
+        else results += ".";
+        cycle++
+        if (cycle>40) cycle = 1
+        x += Number(lineArr[1]);
+      }
+
+    });
+
+    await events.once(rl, "close");
+
+    console.log("results", results);
+    console.log(results.match(/.{1,40}/g))
+
+    // R E H P R L U B
+    //  console.log(values);
   } catch (err) {
     console.error(err);
   }
 })();
+
+
--- day9/index.js
+++ day9/index.js
@@ -2,13 +2,6 @@ const events = require("events");
 const fs = require("fs");
 const readline = require("readline");
 
-let count = 1
-// let positionMap = {
-//   xH: 10000,
-//   yH: 10000,
-//   xT: 10000,
-//   yT: 10000,
-// };
 let positionMap = {
   xH: 0,
   yH: 0,
@@ -16,16 +9,13 @@ let positionMap = {
   yT: 0,
 };
 
-// let pathMapCount = { "10000_10000": 1 };
 let pathMapCount = { "0_0": 1 };
-
-let pathMapCount2 = { 0: { 0: 1 } };
 
 (async function processLineByLine() {
   try {
     const rl = readline.createInterface({
-      input: fs.createReadStream(`${__dirname}/input-basic.txt`),
-      // input: fs.createReadStream(`${__dirname}/input.txt`),
+      // input: fs.createReadStream(`${__dirname}/input-basic.txt`),
+      input: fs.createReadStream(`${__dirname}/input.txt`),
       crlfDelay: Infinity,
     });
 
@@ -42,12 +32,6 @@ let pathMapCount2 = { 0: { 0: 1 } };
 
     await events.once(rl, "close");
 
-    console.log("count");
-    console.log(count);
-    console.log("pathMapCount2");
-    // console.log(pathMapCount2);
-    console.log("pathMapCount");
-    console.log(pathMapCount);
     console.log("answer", Object.keys(pathMapCount).length);
   } catch (err) {
     console.error(err);
@@ -58,7 +42,7 @@ let pathMapCount2 = { 0: { 0: 1 } };
 function moveTail() {
   const { xH, yH, xT, yT } = positionMap;
   // console.log('positionMap',positionMap)
-  let moveDirection = "x";
+  let moveDirection = "";
   let moveValue = 0;
 
   if (xH > xT + 1) {
@@ -79,7 +63,7 @@ function moveTail() {
     moveValue = -1;
   }
 
-  if (moveValue != 0) {
+ if (moveValue != 0) {
     if (moveDirection == "x") {
       positionMap["xT"] += moveValue;
       if (yH > yT) {
@@ -94,28 +78,15 @@ function moveTail() {
       if (xH > xT) {
         positionMap["xT"] += 1;
       } else if (xH < xT) {
-        positionMap["yT"] -= 1;
+        positionMap["xT"] -= 1;
       }
     }
-    if (pathMapCount2[positionMap["xT"]] == undefined){
-      pathMapCount2[positionMap["xT"]] = { [positionMap["yT"]]: 1 };
-      count++
-    }
-      
-    else {
-      if (pathMapCount2[positionMap["xT"]][positionMap["yT"]] == undefined) {
-        pathMapCount2[positionMap["xT"]][positionMap["yT"]] = 1;
-        count++
-      } else {
-        pathMapCount2[positionMap["xT"]][positionMap["yT"]]++;
-        count++
-      }
-    }
-
+   
     const pathLog = `${positionMap["xT"]}_${positionMap["yT"]}`;
     if (pathMapCount[pathLog] == undefined) pathMapCount[pathLog] = 1;
     else pathMapCount[pathLog] += 1;
   }
+ 
 }
 
 function setMoveValues() {}
@@ -123,30 +94,23 @@ function setMoveValues() {}
 function headMoveDirection(line) {
   let moveX = 0;
   let moveY = 0;
-  // const tempMap = { R: { moveX } };
   const lineArr = line.split(" ");
   const steps = Number(lineArr[1]);
   switch (lineArr[0]) {
     case "R":
-      moveX = steps;
+      moveX = 1;
       break;
     case "L":
-      moveX = -1 * steps;
+      moveX = -1;
       break;
     case "U":
-      moveY = -1 * steps;
+      moveY = -1;
       break;
     case "D":
-      moveY = steps;
+      moveY = 1;
       break;
   }
   return { moveX, moveY, steps };
 }
-
-
-
-
-
-
 
 
